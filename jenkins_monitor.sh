4e454be90a8dd1a67b742d19c8b8e86277e26288
Nathan Duckett <nduckett13@gmail.com>
2021-06-08 03:37:01 +0000

    Change to use -k for curl command

--- jenkins_monitor.sh
+++ jenkins_monitor.sh
@@ -16,4 +16,4 @@ fi
 # Remove any spaces in JOB_NAME
 JOB_NAME="$(echo "$JOB_NAME" | sed 's/ /%20/g')"
 
-curl --insecure -q "$JENKINS_URL/job/$JOB_NAME/$EXECUTION_ID/consoleText" | less
+curl -kq "$JENKINS_URL/job/$JOB_NAME/$EXECUTION_ID/consoleText" | less
