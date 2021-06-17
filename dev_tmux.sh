1ea162d224cdfb4d9e0e40e882c02167b78945da
Nathan Duckett <nduckett13@gmail.com>
2021-06-17 21:54:59 +1200

    Fix shellcheck violations

--- dev_tmux.sh
+++ dev_tmux.sh
@@ -7,12 +7,17 @@ if [[ -f "$SCRIPT_DIR/.env" ]]; then
     source "$SCRIPT_DIR/".env
 fi
 
-# Chcek for arguments if provided for a folder directory
+# Default git folders if not set for the search - only if not set in .env
+if [[ -z "$git_folders" ]]; then
+  git_folders="$HOME/Documents/"
+fi
+
+# Check for arguments if provided for a folder directory
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
     items=""
-    for name in ${git_folders[@]}; do
+    for name in "${git_folders[@]}"; do
         items+=$(find "$name" -maxdepth 1 -mindepth 1 -type d)
         # Add new line to ensure each repo is separate
         items+=$'\n'
@@ -29,10 +34,9 @@ fi
 dirname=$(basename "$selected")
 
 # Switch to the tmux session if it exists
-tmux switch-client -t $dirname
-if [[ $? -eq 0 ]]; then
-    exit 0
+if tmux switch-client -t "$dirname"; then
+  exit 0
 fi
 
 # Create new session if it doesn't exist
-tmux new-session -c "$selected" -d -s "$dirname" && tmux switch-client -t "$dirname" || tmux new -c "$selected" -A -s "$dirname"
+tmux new-session -c "$selected" -d -s "$dirname" && (tmux switch-client -t "$dirname" || tmux new -c "$selected" -A -s "$dirname")
