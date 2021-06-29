aa594966fe8093c0285d20284044d2e7468227ef
Nathan Duckett <nduckett13@gmail.com>
2021-06-29 22:07:54 +1200

    Update logic to recursively search - Enables support for checking files in same folder

--- helper/find_in_files.sh
+++ helper/find_in_files.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Find in Files
+# Usage: find_in_files.sh "$PATTERN"
+# Description: Find any files which contain a pattern recursively through all subdirectories.
 
+# Get the string pattern to be searched within the files.
 SEARCH_STRING="$1"
 
-for file in **/*; do
-	if grep -q "$SEARCH_STRING" "$file"; then
-		echo "$file"
-	fi
-done
+# Recursive function definition to search through all the files within
+search() {
+  DIR_NAME="$1"
+  for file in "$DIR_NAME"/*; do
+    if [[ -d "$file" ]]; then
+      search "$file"
+      # Ensure to skip the search on directory
+      continue
+    fi
+    if grep -q "$SEARCH_STRING" "$file"; then
+      echo "$file"
+    fi
+  done
+}
+
+search .
